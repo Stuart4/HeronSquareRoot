98efbf13e0a052d85917dc6468153bea6f3139fb
Jacob Stuart <stuart4@purdue.edu>
2014-10-12 19:41:08 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * HeronSquareRoot
+ *
+ * @author Jacob Stuart
+ * @lab 806
+ * @date October 12, 2014
+ */
+public class Main {
+	public static void main(String[] args) {
+
+	}
+
+	public double calcSquareRoot(double input) {
+		
+	}
+}
