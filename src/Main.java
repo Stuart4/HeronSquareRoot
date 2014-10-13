2f8f424cbc40599870fc9fb7539293b56109b98e
Jacob Stuart <stuart4@purdue.edu>
2014-10-12 20:02:37 -0400

    implements square rooting method

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * HeronSquareRoot
  *
@@ -6,11 +8,38 @@
  * @date October 12, 2014
  */
 public class Main {
-	public static void main(String[] args) {
+	final static double PRECISION = 0.00000001;
+	private static int loops = 0;
 
+	public static void main(String[] args) {
+		Scanner sin = new Scanner(System.in);
+		while (true) {
+			System.out.print("Enter number to calculate square root of: ");
+			if (sin.hasNextDouble()) {
+				System.out.println(calcSquareRoot(sin.nextDouble()));
+			}
+		}
 	}
 
-	public double calcSquareRoot(double input) {
-		
+	public static double calcSquareRoot(double input) {
+		if (input == 0)
+			return 0;
+
+		if (input < 0) {
+			System.err.println("Cannot take the square root of a negative number.");
+			return -1;
+		}
+
+		double guess = input / 31.0;
+		loops = 0;
+
+		while (true) {
+			double lastguess = guess;
+			guess = (lastguess + (input / guess)) / 2;
+			System.out.printf("guess: %f, lastguess: %f, loops: %d\n", guess, lastguess, loops);
+			if (Math.abs(lastguess - guess) <= PRECISION)
+				return guess;
+			loops++;
+		}
 	}
 }
